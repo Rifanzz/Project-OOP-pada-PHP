031c2067e6e7de923ac4a48f20e7c91762afc730
Rifanz <rifanzz@gmail.com>
2023-04-08 07:41:56 +0700

    revisi 3

---
+++ lat4_8.php
@@ -0,0 +1,17 @@
+<?php
+class One {
+    private static $var=0;
+
+    function __construct() {}
+
+    static function disp() {
+        print self::$var;
+    }
+    
+    function __destruct(){}
+
+}
+
+one::disp();
+
+?>
