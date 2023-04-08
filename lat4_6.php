0214103743d2e714f113fa23f27779b12d3e925f
Rifanz <rifanzz@gmail.com>
2023-04-08 07:27:07 +0700

    revisi 2

---
+++ lat4_6.php
@@ -0,0 +1,25 @@
+<?php
+interface a {
+    public function foo();
+}
+
+interface b {
+    public function bar();
+}
+
+interface c extends a, b {
+    public function baz();
+}
+
+class d implements c {
+    public function foo() {}
+    public function bar() {}
+    public function baz() {}
+}
+
+class e implements a, b {
+    public function foo() {}
+    public function bar() {}
+}
+
+?>
